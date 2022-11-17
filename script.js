d7e543fc358d52e0dae8807a7dcb918b808ee5f4
shivanirathod13 <shivanirathod4545@gmail.com>
2022-11-17 11:47:47 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const tempload = () => {
+  let temp = document.getElementById("temp");
+  temp.innerHTML = "&#xf2cb";
+
+  setTimeout(() => {
+    temp.innerHTML = "&#xf2ca";
+    temp.style.color = "#f8b627";
+  }, 1000);
+
+  setTimeout(() => {
+    temp.innerHTML = "&#xf2c9";
+  }, 2000); //1000
+
+  setTimeout(() => {
+    temp.innerHTML = "&#xf2c8";
+  }, 3000); //1000
+
+  setTimeout(() => {
+    temp.innerHTML = "&#xf2c7";
+    temp.style.color = "#d63031";
+  }, 4000); //1000
+};
+
+tempload();
+//infiniti loop
+
+setInterval(tempload, 5000);
